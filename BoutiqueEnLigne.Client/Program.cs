cb4c90c0ee787ad217e1b7709c238871cece6434
Fatou G <sophia-gueye@outlook.fr>
2022-12-16 12:13:19 +0100

    Ajout pages

--- BoutiqueEnLigne.Client/Program.cs
+++ BoutiqueEnLigne.Client/Program.cs
@@ -2,12 +2,17 @@ using BoutiqueEnLigne.Core.Repositories;
 using BoutiqueEnLigne.Core.Services;
 using BoutiqueEnLigne.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using BoutiqueEnLigne.Client.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("connexion")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<BoutiqueEnLigneClientContext>();
 //builer.Configuration["ParamTest"];
 //builder.Services.AddScoped<IExempleService, ExempleService>();
 // Enregistrer les services ici
@@ -28,6 +33,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
